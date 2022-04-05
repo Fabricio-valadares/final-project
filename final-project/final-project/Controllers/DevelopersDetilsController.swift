d2df32af48a5ae04b7e3cbd7027afa4ac7784437
Melquias Ribeiro <melquias.ribeiro@meliuz.com.br>
2022-04-04 23:37:20 -0300

    Revert "adicionando o hiperlink na tela de detalhes dos devs"

--- final-project/final-project/Controllers/DevelopersDetilsController.swift
+++ final-project/final-project/Controllers/DevelopersDetilsController.swift
@@ -1,8 +1,6 @@
 import UIKit
-import SafariServices
-import MessageUI
 
-class DevelopersDetilsController: UIViewController, MFMailComposeViewControllerDelegate {
+class DevelopersDetilsController: UIViewController {
     
     private let developerPhone :String
     private let developerEmail :String
@@ -49,98 +47,23 @@ class DevelopersDetilsController: UIViewController, MFMailComposeViewControllerD
         return subTitle
     }()
     
-    lazy var buttonPhone: UIButton = {
-        buttonPhone = UIButton(type: .custom)
-        buttonPhone.setTitle(self.developerPhone, for: .normal)
-        buttonPhone.setTitleColor(UIColor.black, for: .normal)
-        buttonPhone.backgroundColor = .white
-        buttonPhone.isUserInteractionEnabled = true
-        buttonPhone.addTarget(self, action:  #selector(buttonActionPhone), for: .touchUpInside)
-   
-   
-    return buttonPhone
-    
-}()
-    
-    @objc func buttonActionPhone(sender: UIButton!) {
-        let flag = Phone().btnCallClick(developerPhone)
-        alert(flag: flag)
-    
-       print("Button Phone")
-    }
-    
-    lazy var buttonEmail: UIButton = {
-        buttonEmail = UIButton(type: .custom)
-        buttonEmail.setTitle(self.developerEmail, for: .normal)
-        buttonEmail.setTitleColor(UIColor.black, for: .normal)
-        buttonEmail.backgroundColor = .white
-        buttonEmail.isUserInteractionEnabled = true
-        buttonEmail.addTarget(self, action:  #selector(buttonActionEmail), for: .touchUpInside)
-   
-   
-    return buttonEmail
-    
-}()
-    
-    @objc func buttonActionEmail(sender: UIButton!) { //OLHAR
-      sendEmail(developerEmail)
-      print("Button Email")
-    }
-    
-    lazy var buttonTwitter: UIButton = {
-        buttonTwitter = UIButton(type: .custom)
-        buttonTwitter.setTitle(self.developerTwitter, for: .normal)
-        buttonTwitter.setTitleColor(UIColor.black, for: .normal)
-        buttonTwitter.backgroundColor = .white
-        buttonTwitter.isUserInteractionEnabled = true
-        buttonTwitter.addTarget(self, action:  #selector(buttonActionTwitter), for: .touchUpInside)
-   
-   
-    return buttonTwitter
-    
-}()
-    
-    @objc func buttonActionTwitter(sender: UIButton!) {
-      callSafari(developerTwitter)
-      print("Button Twitter")
-    }
-    
-    lazy var buttonLinkedin: UIButton = {
-        buttonLinkedin = UIButton(type: .custom)
-        buttonLinkedin.setTitle(self.developerLinkedin, for: .normal)
-        buttonLinkedin.setTitleColor(UIColor.black, for: .normal)
-        buttonLinkedin.backgroundColor = .white
-        buttonLinkedin.isUserInteractionEnabled = true
-        buttonLinkedin.titleLabel?.numberOfLines = 0
-        buttonLinkedin.addTarget(self, action:  #selector(buttonActionLinkedin), for: .touchUpInside)
-   
-   
-    return buttonLinkedin
-    
-}()
-    
-    @objc func buttonActionLinkedin(sender: UIButton!) {
-      callSafari(developerLinkedin)
-      print("Button Linkedin")
-    }
-    
     lazy var topicPhone:UIView = {
-        let topicPhone = Utilities().createTopicItemWithButton(icon: "phone", key: "Telefone: ", value: self.developerPhone, button: buttonPhone)
+        let topicPhone = Utilities().createTopicItem(icon: "phone", key: "Telefone: ", value: self.developerPhone)
         return topicPhone
     }()
-
+    
     lazy var topicEmail:UIView = {
-        let topicEmail = Utilities().createTopicItemWithButton(icon: "envelope", key: "Email: ", value: self.developerEmail, button: buttonEmail)
+        let topicEmail = Utilities().createTopicItem(icon: "envelope", key: "Email: ", value: self.developerEmail)
         return topicEmail
     }()
-
+    
     lazy var topicTwetter:UIView = {
-        let topicTwetter = Utilities().createTopicItemWithButton(icon: "network", key: "Twitter: ", value: self.developerTwitter, button: buttonTwitter)
+        let topicTwetter = Utilities().createTopicItem(icon: "network", key: "Twetter: ", value: self.developerTwitter)
         return topicTwetter
     }()
-
+    
     lazy var topicLinkedin:UIView = {
-        let topicLinkedin = Utilities().createTopicItemWithButton(icon: "network", key: "Linkedin: ", value: self.developerLinkedin, button: buttonLinkedin)
+        let topicLinkedin = Utilities().createTopicItem(icon: "network", key: "Linkedin: ", value: self.developerLinkedin)
         return topicLinkedin
     }()
     
@@ -183,64 +106,9 @@ class DevelopersDetilsController: UIViewController, MFMailComposeViewControllerD
             
             topicsStack.anchor(top: subTitle.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor,paddingTop: 16, paddingLeft: 16, paddingRight: 16)
         }
-    }
-    
-    // MARK: - Call Button
-    
-   private func sendEmail(_ email: String) {
-        if MFMailComposeViewController.canSendMail() {
-            let mail = MFMailComposeViewController()
-            mail.mailComposeDelegate = self
-            mail.setToRecipients([email])
-            mail.setMessageBody("<p>Obrigada Doug e Idwall!!!</p>", isHTML: true)
-
-            present(mail, animated: true)
-        } else {
-           alert(flag: false)
-        }
-    }
-
-    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
-        controller.dismiss(animated: true)
-    }
-
-    private func callSafari(_ url: String) {
-        if let url = URL(string: url) {
-            
-            let config = SFSafariViewController.Configuration()
-            config.entersReaderIfAvailable
-            
-            let DevelopersDetilsController = SFSafariViewController(url: url, configuration: config)
-            DevelopersDetilsController.delegate = self
-            DevelopersDetilsController.preferredControlTintColor = .black
-            DevelopersDetilsController.preferredBarTintColor = .systemGray
-            DevelopersDetilsController.dismissButtonStyle = .close
-            present(DevelopersDetilsController, animated: true)
-        }
-    }
-    
-    private func alert(flag: Bool) {
         
-        if flag == false {
-        var dialogMessage = UIAlertController(title: "Erro", message: "Não é possível executar essa ação no simulador", preferredStyle: .alert)
-        let noPossibleCall = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
-        })
-        dialogMessage.addAction(noPossibleCall)
-        self.present(dialogMessage, animated: true, completion: nil)}
+        
+        
+        
     }
 }
-
-extension DevelopersDetilsController: SFSafariViewControllerDelegate {
-    
-    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
-        controller.dismiss(animated: true) {
-            print("Fazer alguma coisa")
-        }
-    }
-    
-    func safariViewController(_ controller: SFSafariViewController, didCompleteInitialLoad didLoadSuccessfully: Bool) {
-        
-            print("didCompleteInitialLoad \(didLoadSuccessfully)")
-    }
-}
-
